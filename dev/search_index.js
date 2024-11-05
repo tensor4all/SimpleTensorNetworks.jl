9a316cb6a73ce07ee10fe7ab8537a4c79b7b1f0e
Documenter.jl <documenter@juliadocs.github.io>
2024-11-05 14:45:37 +0000

    build based on fc68e1c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SimpleTensorNetworks","category":"page"},{"location":"#SimpleTensorNetworks","page":"Home","title":"SimpleTensorNetworks","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"This is the documentation for SimpleTensorNetworks.","category":"page"}]
+}
